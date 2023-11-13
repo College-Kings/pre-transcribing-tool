273f9ea1f8ad60c79ae7476c33374be560e1cf15
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-11-13 23:07:36 +0000

    Add constants for header keys and valid scene statements.

---
+++ src-tauri/src/render_table_creator/config.rs
@@ -0,0 +1,12 @@
+pub const HEADER_KEYS: [&str; 8] = [
+    "Writer",
+    "Scene Number",
+    "Location",
+    "MC Outfit",
+    "Outfit",
+    "Day",
+    "Time",
+    "TR",
+];
+
+pub const VALID_SCENE_STATEMENTS: [&str; 2] = ["scene", "show"];
