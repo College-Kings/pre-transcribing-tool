63aa12587d598361653f22eac4a7c056b637e6a9
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-02-03 03:31:59 +0000

    remove hard coded values

--- src-tauri/src/render_table_creator/config.rs
+++ src-tauri/src/render_table_creator/config.rs
@@ -1,14 +1,1 @@
-pub const HEADER_KEYS: [&str; 8] = [
-    "Writer",
-    "Scene Number",
-    "Location",
-    "MC Outfit",
-    "Outfit",
-    "Day",
-    "Time",
-    "TR",
-];
-
-pub const VALID_SCENE_STATEMENTS: [&str; 2] = ["scene", "show"];
-
 pub const IGNORE_SCENES: [&str; 1] = ["black"];
