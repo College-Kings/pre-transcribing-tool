1f64ea7e8fbb2f11d5c8e6ffe38885e03e0ab893
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-02-21 09:19:12 +0000

    Add FRAT_SCENE_REGEX to regexes.rs

--- src-tauri/src/regexes.rs
+++ src-tauri/src/regexes.rs
@@ -6,6 +6,7 @@ lazy_static! {
     pub static ref OUTFIT_HEADER_REGEX: Regex = Regex::new(r#"^# Outfit: (\w+),$"#).unwrap();
     pub static ref HEADER_REGEX: Regex = Regex::new(r"^#\s*(.+):\s*(.+)").unwrap();
     pub static ref SCENE_REGEX: Regex = Regex::new(r"^(scene|show)\s+(\S+)(.*)").unwrap();
+    pub static ref FRAT_SCENE_REGEX: Regex = Regex::new(r"^(frat_scene)\s+(\S+)(.*)").unwrap();
     pub static ref SCENE_DESCRIPTION_REGEX: Regex = Regex::new(r".+#\s*(.+)$").unwrap();
     pub static ref IMAGE_REGEX: Regex = Regex::new(r"^image\s+(\S+)\s*=(.+)").unwrap();
     pub static ref IMAGE_DESCRIPTION_REGEX: Regex = Regex::new(r#""\s*(.+)"$"#).unwrap();
