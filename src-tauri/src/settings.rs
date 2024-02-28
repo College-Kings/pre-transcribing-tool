0d702344208f00553461360e6618415524d58f39
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-02-28 09:01:53 +0000

    Refactor settings module and remove unused speakers module

--- src-tauri/src/settings.rs
+++ src-tauri/src/settings.rs
@@ -1,24 +1,98 @@
-use crate::error::Error;
+use crate::{
+    error::Error, regexes::CHARACTER_DEFINITION_REGEX, utils::find_game_folder, SETTINGS_PATH,
+};
 use serde::{Deserialize, Serialize};
-use std::{fs, path::PathBuf, sync::Mutex};
+use std::{
+    collections::HashMap,
+    fs,
+    path::{Path, PathBuf},
+    sync::Mutex,
+};
 use tauri::State;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Settings {
     pub episode: Mutex<i32>,
     pub selected_file: Mutex<Option<PathBuf>>,
     pub selected_folder: Mutex<Option<PathBuf>>,
     pub scene_number: Mutex<Option<String>>,
+
+    pub speakers: Mutex<HashMap<String, String>>,
 }
 
 impl Settings {
-    pub fn new(episode: i32) -> Self {
-        Self {
+    pub fn new(episode: i32) -> Result<Self, Error> {
+        if let Ok(settings) = Self::load() {
+            return Ok(settings);
+        }
+
+        let settings = Self {
             episode: Mutex::new(episode),
             selected_file: Mutex::new(None),
             selected_folder: Mutex::new(None),
             scene_number: Mutex::new(None),
+
+            speakers: Mutex::new(HashMap::new()),
+        };
+
+        settings.save()?;
+
+        Ok(settings)
+    }
+
+    pub fn save(&self) -> Result<(), Error> {
+        fs::write(SETTINGS_PATH.as_path(), serde_json::to_string(&self)?)?;
+        Ok(())
+    }
+
+    pub fn load() -> Result<Self, Error> {
+        let settings = fs::read_to_string(SETTINGS_PATH.as_path())?;
+        Ok(serde_json::from_str(&settings)?)
+    }
+
+    pub fn update_speakers(&self, file_path: &Path) -> Result<(), Error> {
+        let character_file = find_game_folder(file_path)
+            .ok_or("Unable to find game folder")?
+            .join("characters.rpy");
+
+        let lines: Vec<String> = std::fs::read_to_string(character_file)?
+            .lines()
+            .map(|line| line.to_string())
+            .collect();
+
+        let mut speakers: HashMap<String, String> = HashMap::new();
+
+        for line in lines {
+            let captures = match CHARACTER_DEFINITION_REGEX.captures(&line) {
+                Some(captures) => captures,
+                None => continue,
+            };
+
+            let character_var = captures
+                .get(1)
+                .unwrap()
+                .as_str()
+                .trim_start_matches("character.");
+            let character_name = captures.get(2).unwrap().as_str();
+
+            speakers.insert(character_var.into(), character_name.into());
         }
+
+        let mut speakers_guard = self.speakers.lock().unwrap();
+        *speakers_guard = speakers;
+
+        Ok(())
+    }
+
+    pub fn get_speakers(&self, file_path: &Path) -> Result<HashMap<String, String>, Error> {
+        let _ = self.update_speakers(file_path);
+        let speakers = self.speakers.lock().unwrap();
+
+        if speakers.is_empty() {
+            return Err("No speakers found".into());
+        }
+
+        Ok(speakers.clone())
     }
 }
 
@@ -31,26 +105,4 @@ pub fn get_episode_number(settings: State<Settings>) -> i32 {
 pub fn set_episode_number(settings: State<Settings>, value: i32) {
     *settings.episode.lock().unwrap() = value;
     println!("Episode number set to: {}", value)
-}
-
-pub fn initialise_settings() -> Result<Settings, Error> {
-    let settings_file = dirs::config_local_dir()
-        .unwrap()
-        .join("Viridian")
-        .join("TranscribingTool")
-        .join("settings.json");
-
-    if settings_file.exists() {
-        let settings = fs::read_to_string(&settings_file)?;
-        let settings: Settings = serde_json::from_str(&settings).unwrap_or(Settings::new(1));
-
-        return Ok(settings);
-    }
-
-    let settings = Settings::new(1);
-
-    fs::create_dir_all(settings_file.parent().unwrap())?;
-    fs::write(&settings_file, serde_json::to_string(&settings)?)?;
-
-    Ok(settings)
 }
