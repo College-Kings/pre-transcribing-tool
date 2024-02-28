ec31fb53e3d6cadc27c119b92a862f64fd652cd8
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-02-28 09:02:09 +0000

    Remove unused module and initialize settings directory

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,7 +5,6 @@ mod error;
 mod regexes;
 mod render_table_creator;
 mod settings;
-mod speakers;
 mod transcribing_formatter;
 mod utils;
 mod writing_formatter;
@@ -14,13 +13,20 @@ use commands::{
     create_render_table, file_dialogue, run_transcribing_formatter, run_writing_formatter,
 };
 use error::Error;
-use settings::{get_episode_number, initialise_settings, set_episode_number};
+use lazy_static::lazy_static;
+use settings::{get_episode_number, set_episode_number, Settings};
+use std::{fs, path::PathBuf};
+
+lazy_static! {
+    pub static ref DATA_ROOT: PathBuf = dirs::config_local_dir().unwrap().join("CollegeKingsTool");
+    pub static ref SETTINGS_PATH: PathBuf = DATA_ROOT.join("settings.json");
+}
 
 fn main() -> Result<(), Error> {
-    let settings = initialise_settings();
+    fs::create_dir_all(DATA_ROOT.as_path())?;
 
     tauri::Builder::default()
-        .manage(settings?)
+        .manage(Settings::new(1)?)
         .invoke_handler(tauri::generate_handler![
             file_dialogue,
             run_writing_formatter,
