42d1300ba29d92272ad1c729a2b3cb4d72ade97f
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-02-28 09:37:45 +0000

    Improve code formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,68 @@
 <script lang="ts">
-    import ToolUI from "./lib/ToolUI.svelte";
+  import ToolUI from "./lib/ToolUI.svelte";
 
-    let currentTool = "writingFormatter";
+  let currentTool = "writingFormatter";
 
-    function updateTool(tool: string) {
-        currentTool = tool;
-    }
+  function updateTool(tool: string) {
+    currentTool = tool;
+  }
 </script>
 
 <main class="container">
   <div class="app_container">
     <div class="tools">
-      <button class:button-selected={currentTool === "writingFormatter"} on:click={() => updateTool("writingFormatter")}>Writing Formatter</button>
-      <button class:button-selected={currentTool === "transcribingFormatter"} on:click={() => updateTool("transcribingFormatter")}>Transcribing Formatter</button>
-      <button class:button-selected={currentTool === "renderTableCreator"} on:click={() => updateTool("renderTableCreator")}>Render Table Creator</button>
+      <button
+        class:button-selected={currentTool === "writingFormatter"}
+        on:click={() => updateTool("writingFormatter")}
+        >Writing Formatter</button
+      >
+      <button
+        class:button-selected={currentTool === "transcribingFormatter"}
+        on:click={() => updateTool("transcribingFormatter")}
+        >Transcribing Formatter</button
+      >
+      <button
+        class:button-selected={currentTool === "renderTableCreator"}
+        on:click={() => updateTool("renderTableCreator")}
+        >Render Table Creator</button
+      >
     </div>
     <div class="toolUI">
-      <ToolUI {currentTool}/>
+      <ToolUI {currentTool} />
     </div>
   </div>
 </main>
 
 <style>
-    .app_container {
-        display: flex;
-    }
+  .app_container {
+    display: flex;
+  }
 
-    .tools {
-        flex: 1;
-        /*background-color: lightblue;*/
-        padding: 10px;
-    }
+  .tools {
+    flex: 1;
+    /*background-color: lightblue;*/
+    padding: 10px;
+  }
 
-    .toolUI {
-        flex: 3;
-        /*background-color: green;*/
-        padding: 10px;
-    }
+  .toolUI {
+    flex: 3;
+    /*background-color: green;*/
+    padding: 10px;
+  }
 
-    /*#fileFormatterButton {*/
-    /*    background-color: transparent;*/
-    /*    border: 1px solid white;*/
-    /*    color: white;*/
-    /*    padding: 10px 20px;*/
-    /*    font-size: 16px;*/
-    /*    cursor: pointer;*/
-    /*    border-radius: 5px;*/
-    /*}*/
+  /*#fileFormatterButton {*/
+  /*    background-color: transparent;*/
+  /*    border: 1px solid white;*/
+  /*    color: white;*/
+  /*    padding: 10px 20px;*/
+  /*    font-size: 16px;*/
+  /*    cursor: pointer;*/
+  /*    border-radius: 5px;*/
+  /*}*/
 
-    /*!* Optional: Add hover effect *!*/
-    /*#fileFormatterButton:hover {*/
-    /*    background-color: white;*/
-    /*    color: black;*/
-    /*}*/
-
+  /*!* Optional: Add hover effect *!*/
+  /*#fileFormatterButton:hover {*/
+  /*    background-color: white;*/
+  /*    color: black;*/
+  /*}*/
 </style>
