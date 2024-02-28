2c8f33e9953a9c2286f066b4505a9391bc171cc8
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-02-28 09:01:59 +0000

    Refactor get_files_from_dir function and add find_game_folder function

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -6,5 +6,17 @@ pub fn get_files_from_dir<P: AsRef<Path>>(path: P) -> Vec<PathBuf> {
         .expect("Unable to read directory")
         .filter_map(|entry| entry.ok())
         .map(|entry| entry.path())
-        .collect::<Vec<PathBuf>>()
+        .collect()
+}
+
+pub fn find_game_folder(mut current_path: &Path) -> Option<PathBuf> {
+    while let Some(parent) = current_path.parent() {
+        if parent.ends_with("game") {
+            return Some(parent.to_path_buf());
+        }
+
+        current_path = parent;
+    }
+
+    None
 }
